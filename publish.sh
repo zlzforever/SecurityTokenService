8d7cd5fd0b839622fce8ee5fc5a23c6b356a2752
Lewis <zlzforever@163.com>
2022-05-13 11:58:38 +0800

    update

--- publish.sh
+++ publish.sh
@@ -1,11 +1,2 @@
-cd src/SecurityTokenService && yarn install
-dotnet publish --os linux -c Release -o security_token_service_out
-rm -rf security_token_service_out/wwwroot/css/site.css
-rm -rf security_token_service_out/wwwroot/js/site.js
-rm -rf security_token_service_out/sts.json
-(cd security_token_service_out/wwwroot/lib/bootstrap && rm -rf `ls | grep -v "dist"`) && return
-(cd security_token_service_out/wwwroot/lib/jquery && rm -rf `ls | grep -v "dist"`) && return
-(cd security_token_service_out/wwwroot/lib/jquery-validation && rm -rf `ls | grep -v "dist"`) && return
-(cd security_token_service_out/wwwroot/lib/jquery-validation-unobtrusive && rm -rf `ls | grep -v "dist"`) && return
 docker build -t zlzforever/security-token-service .
 docker push zlzforever/security-token-service
--- publish_m1.sh
+++ publish_m1.sh
@@ -3,9 +3,9 @@ dotnet publish --os linux -c Release -o security_token_service_out
 rm -rf security_token_service_out/wwwroot/css/site.css
 rm -rf security_token_service_out/wwwroot/js/site.js
 rm -rf security_token_service_out/sts.json
-(cd security_token_service_out/wwwroot/lib/bootstrap && rm -rf `ls | grep -v "dist"`) && return
-(cd security_token_service_out/wwwroot/lib/jquery && rm -rf `ls | grep -v "dist"`) && return
-(cd security_token_service_out/wwwroot/lib/jquery-validation && rm -rf `ls | grep -v "dist"`) && return
-(cd security_token_service_out/wwwroot/lib/jquery-validation-unobtrusive && rm -rf `ls | grep -v "dist"`) && return
+#(cd security_token_service_out/wwwroot/lib/bootstrap && rm -rf `ls | grep -v "dist"`) && return
+#(cd security_token_service_out/wwwroot/lib/jquery && rm -rf `ls | grep -v "dist"`) && return
+#(cd security_token_service_out/wwwroot/lib/jquery-validation && rm -rf `ls | grep -v "dist"`) && return
+#(cd security_token_service_out/wwwroot/lib/jquery-validation-unobtrusive && rm -rf `ls | grep -v "dist"`) && return
 docker buildx build --platform linux/amd64 -t zlzforever/security-token-service .
 docker push zlzforever/security-token-service
