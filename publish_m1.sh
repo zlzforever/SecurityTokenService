d2ff26723ab48e21abd5d0d12d6fa571fd126a49
Lewis <zlzforever@163.com>
2022-05-13 08:59:40 +0800

    修改打包脚本

--- publish_m1.sh
+++ publish_m1.sh
@@ -1,3 +1,11 @@
-sts_image=zlzforever/security-token-service
-docker buildx build --platform linux/amd64 -t $sts_image .
-docker push $sts_image
+cd src/SecurityTokenService && yarn install
+dotnet publish --os linux -c Release -o security_token_service_out
+rm -rf security_token_service_out/wwwroot/css/site.css
+rm -rf security_token_service_out/wwwroot/js/site.js
+rm -rf security_token_service_out/sts.json
+(cd security_token_service_out/wwwroot/lib/bootstrap && rm -rf `ls | grep -v "dist"`) && return
+(cd security_token_service_out/wwwroot/lib/jquery && rm -rf `ls | grep -v "dist"`) && return
+(cd security_token_service_out/wwwroot/lib/jquery-validation && rm -rf `ls | grep -v "dist"`) && return
+(cd security_token_service_out/wwwroot/lib/jquery-validation-unobtrusive && rm -rf `ls | grep -v "dist"`) && return
+docker buildx build --platform linux/amd64 -t zlzforever/security-token-service .
+docker push zlzforever/security-token-service
