152c75de1733a7f9c29df3915cc97beef7c60631
Lewis Zou <zlzforever@163.com>
2024-07-26 09:17:46 +0800

    打印 IDS4 配置日志

--- src/SecurityTokenService/IdentityServer/PublicFacingUrlMiddleware.cs
+++ src/SecurityTokenService/IdentityServer/PublicFacingUrlMiddleware.cs
@@ -2,6 +2,8 @@ using System.Threading.Tasks;
 using IdentityServer4.Extensions;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 
 namespace SecurityTokenService.IdentityServer;
 
@@ -9,27 +11,32 @@ public class PublicFacingUrlMiddleware(RequestDelegate next, IConfiguration conf
 {
     public async Task Invoke(HttpContext context)
     {
+        var logger = context.RequestServices.GetRequiredService<ILogger<PublicFacingUrlMiddleware>>();
         var schema = configuration["IdentityServer:Scheme"];
         if (schema == "http")
         {
             context.Request.Scheme = "http";
             context.Request.IsHttps = false;
+            logger.LogDebug("Setting scheme to http");
         }
         else if (schema == "https")
         {
             context.Request.Scheme = "https";
             context.Request.IsHttps = true;
+            logger.LogDebug("Setting scheme to https");
         }
 
         var basePath = configuration["IdentityServer:BasePath"];
         if (!string.IsNullOrEmpty(basePath))
         {
+            logger.LogDebug("Setting base path to {BasePath}", basePath);
             context.SetIdentityServerBasePath(basePath);
         }
 
         var origin = configuration["IdentityServer:Origin"];
         if (!string.IsNullOrEmpty(origin))
         {
+            logger.LogDebug("Setting base path to {Origin}", origin);
             context.SetIdentityServerOrigin(origin);
         }
 
