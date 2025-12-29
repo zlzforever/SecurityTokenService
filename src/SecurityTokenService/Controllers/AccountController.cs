917c561cbd1624c0c0923004085f8e0b1fff453c
jonechenug <jonechenug@gmail.com>
2025-12-29 09:46:33 +0800

    调整日志

--- src/SecurityTokenService/Controllers/AccountController.cs
+++ src/SecurityTokenService/Controllers/AccountController.cs
@@ -397,7 +397,7 @@ public class AccountController(
             var availableResult = await CheckUserAvailableAsync(user);
             if (availableResult != null)
             {
-                logger.LogWarning($"{input.PhoneNumber} 手机用户:{availableResult.Message}");
+                logger.LogWarning($"{input.PhoneNumber} 手机用户:{availableResult?.Message ?? "验证失败"}");
                 switch (availableResult.Code)
                 {
                     case Errors.IdentityUserIsNotExist:
